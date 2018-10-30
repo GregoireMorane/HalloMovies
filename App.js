1d1c2b34a4f8bee6261f8187ae032cef6d8322b5
GregoireMorane <gh.morane@gmail.com>
2018-10-30 10:46:41 +0100

    ++Searchbar

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, 
+  Text, 
+  View,
+  Button,
+  TextInput,
+  ScrollView
+} from 'react-native';
 
 export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      userInput: '',
+      finalSearch: null,
+    }
+  }
+  handleSubmit = (event) => {
+    this.setState({finalSearch: event})
+    this.setState({userInput: event})
+    console.log('submit', this.state)
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+          <View style={styles.container}>
+            <Text style={styles.textH}>HalloMovies</Text>
+            <TextInput
+                style={styles.searchbar}
+                placeholder="Your Film..."
+                value={this.state.userInput}
+            />
+            <Button title="Go !" onPress={() => this.handleSubmit(this.state.userInput)} />
+          </View>
+        </ScrollView>
     );
   }
 }
@@ -18,4 +44,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+  searchbar: {
+    padding: 20,
+    //backgroundColor: '#3399FF',
+    width: 300,
+    textAlign: "center",
+  },
+	contentContainer: {
+    flex: 1,
+    paddingVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textH: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  }
 });
